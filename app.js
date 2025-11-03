cb630a73f53e2c92a9c87b7a530b06b6cf85d66f
Aarjav Jain <aarjav1512@gmail.com>
2025-11-03 20:54:58 +0530

    Redirect root page to /listings for better UX

--- app.js
+++ app.js
@@ -23,7 +23,7 @@
     app.use(express.static(path.join(__dirname,"/public")));    
 
     app.get("/", (req, res) => {
-        res.send("Hi, I am root");
+        res.redirect("/listings");
     });
 
     app.set("view engine", "ejs");
